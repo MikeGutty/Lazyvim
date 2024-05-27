8d2dcd44880c49e1d76575a4ca1820c04a43d62f
Michael Gutierrez <michael777gutty@gmail.com>
2024-05-26 21:40:35 -0400

    Update colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,6 @@
 return {
   -- add gruvbox
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = { flavour = "macchiato" } },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = { flavour = "frappe" } },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
