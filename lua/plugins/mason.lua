ad322fbc46ccbff5bc8c9f6a7829d0bc3b20a3ec
Mike <michael777gutty@gmail.com>
2024-07-26 15:41:11 -0400

    Clear treesitter and mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,18 +3,8 @@ return {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
-        "stylua",
         "shellcheck",
-        "shfmt",
         "flake8",
-        "dockerfile-language-server",
-        "docker-compose-language-service",
-        "typescript-language-server",
-        "angular-language-server",
-        "vue-language-server",
-        "python-lsp-server",
-        "ruby-lsp",
-        "rust-analyzer",
       },
     },
   },
