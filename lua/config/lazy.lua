521b9256515982367f075a12f4b2b9868fe8f90d
Mike <michael777gutty@gmail.com>
2025-05-09 10:22:37 -0400

    fix: removing files

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -37,7 +37,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
     -- AI
-    { import = "lazyvim.plugins.extras.ai.codeium" },
+    -- { import = "lazyvim.plugins.extras.ai.codeium" },
     -- import/override with your plugins
     { import = "plugins" },
   },
